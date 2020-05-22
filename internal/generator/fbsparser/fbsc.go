b102dc5d23bcf5fea09218f84c1e1ff8c72c1952
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-05-22 07:47:57 +0200

    flatbuffers schema reader fix, finish tests

--- internal/generator/fbsparser/fbsc.go
+++ internal/generator/fbsparser/fbsc.go
@@ -26,17 +26,8 @@ import (
 	"errors"
 	"fmt"
 	"github.com/objectbox/objectbox-go/internal/generator/fbsparser/reflection"
-	"reflect"
 	"unsafe"
 )
-
-// TODO deduplicate, this is a copy from objectbox/c-arrays
-func cVoidPtrToByteSlice(data unsafe.Pointer, size int, bytes *[]byte) {
-	header := (*reflect.SliceHeader)(unsafe.Pointer(bytes))
-	header.Data = uintptr(data)
-	header.Len = size
-	header.Cap = size
-}
 
 func ParseSchemaFile(filename string) (*reflection.Schema, error) {
 	var cFilename = C.CString(filename)
@@ -52,7 +43,8 @@ func ParseSchemaFile(filename string) (*reflection.Schema, error) {
 	}
 	defer C.fbs_schema_free(fbsBytes)
 
-	var bytes []byte
-	cVoidPtrToByteSlice(fbsBytes.data, int(fbsBytes.size), &bytes)
+	// make a copy of the bytes because the source is deallocated by fbs_schema_free
+	var bytes []byte = C.GoBytes(fbsBytes.data, C.int(fbsBytes.size))
+
 	return reflection.GetRootAsSchema(bytes, 0), nil
 }
--- internal/generator/fbsparser/fbsc_test.go
+++ internal/generator/fbsparser/fbsc_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/objectbox/objectbox-go/test/assert"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -15,19 +16,19 @@ func TestFbsSchemaParser(t *testing.T) {
 
 	file, err := ioutil.TempFile("", "fbs-test")
 	assert.NoErr(t, err)
-	defer os.Remove(file.Name())
+	defer func() {
+		assert.NoErr(t, os.Remove(file.Name()))
+	}()
 
-	_, err = file.WriteString(`// Example schema file
-// Note the property comments - they test multiple different formats
-
-namespace FbsC;
-
+	_, err = file.WriteString(`
 enum Planet:byte { Mercury = 0, Venus, Earth = 2 }
 
+/// A real or imaginary living creature or entity
+/// Note: name may be nil
 table Being {
   age:short = 150;
-  health:short = 100; //< Current health points
-  name:string; // Full being name
+  health:short = 100;
+  name:string;
   friendly:bool = false (deprecated);
   location:Planet = Earth;
 
@@ -50,9 +51,32 @@ root_type Being;`)
 	assert.Eq(t, 1, schema.EnumsLength())
 	assert.Eq(t, 2, schema.ObjectsLength())
 
-	var enum *reflection.Enum
-	assert.True(t, schema.Enums(enum, 0))
-	assert.True(t, enum != nil)
+	var enum reflection.Enum
+	assert.True(t, schema.Enums(&enum, 0))
 	assert.Eq(t, "Planet", string(enum.Name()))
+	assert.Eq(t, 3, enum.ValuesLength())
 
+	var enumVal reflection.EnumVal
+	assert.True(t, enum.Values(&enumVal, 2))
+	assert.Eq(t, "Earth", string(enumVal.Name()))
+
+	var object reflection.Object
+	assert.True(t, schema.Objects(&object, 1))
+	assert.Eq(t, "Item", string(object.Name()))
+	assert.Eq(t, 0, object.DocumentationLength())
+
+	assert.True(t, schema.RootTable(&object) == &object)
+	assert.Eq(t, "Being", string(object.Name()))
+
+	assert.Eq(t, 2, object.DocumentationLength())
+	assert.Eq(t, "A real or imaginary living creature or entity", strings.TrimSpace(string(object.Documentation(0))))
+	assert.Eq(t, "Note: name may be nil", strings.TrimSpace(string(object.Documentation(1))))
+
+	var field reflection.Field
+	assert.Eq(t, 6, object.FieldsLength())
+	assert.True(t, object.Fields(&field, 1)) // sorted by name
+	assert.Eq(t, "belongings", string(field.Name()))
+
+	assert.Eq(t, 1, field.DocumentationLength())
+	assert.Eq(t, "All worldly belongings of this being", strings.TrimSpace(string(field.Documentation(0))))
 }
