eaf5905fb17903e7c1a6da3505ead539d95e5594
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-05-25 17:57:27 +0200

    c++ generated header compilation issues

--- internal/generator/c/templates/model.go
+++ internal/generator/c/templates/model.go
@@ -47,8 +47,8 @@ OBX_model* create_obx_model() {
 		// {{$entity.Name}}
 		if (!obx_model_entity(model, "{{$entity.Name}}", {{$entity.Id.GetId}}, {{$entity.Id.GetUid}})) break;
 		{{range $property := $entity.Properties -}}
-		if (!obx_model_property(model, "{{$property.Name}}", {{$property.Type}}, {{$property.Id.GetId}}, {{$property.Id.GetUid}})) break;
-		{{with $property.Flags}}if (!obx_model_property_flags(model, {{.}})) break;
+		if (!obx_model_property(model, "{{$property.Name}}", OBXPropertyType({{$property.Type}}), {{$property.Id.GetId}}, {{$property.Id.GetUid}})) break;
+		{{with $property.Flags}}if (!obx_model_property_flags(model, OBXPropertyFlags({{.}}))) break;
 		{{end -}}
 		{{end -}}
 		if (!obx_model_entity_last_property_id(model, {{$entity.LastPropertyId.GetId}}, {{$entity.LastPropertyId.GetUid}})) break;
