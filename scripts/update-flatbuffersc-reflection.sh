684031f463b21355ba81092eb596261cb5f69531
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-06-12 17:12:52 +0200

    added a script to update generated flatbuffersc/reflection/*.go

---
+++ scripts/update-flatbuffersc-reflection.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This file is used to regenerate FlatBuffers reflection Go sources in our flatbuffersc Go wrapper
+
+scriptDir=$(dirname "${BASH_SOURCE[0]}")
+repoDir=$(realpath "${scriptDir}/../")
+tmpDir="${repoDir}"/scripts/tmp-generated
+
+# Note: we're using objectbox-generator's flatc-integration.
+# If you can't build objectbox-generator, you can build and use flatbuffers flatc binary instead.
+flatc="go run ./cmd/objectbox-generator FLATC"
+
+if [[ "${repoDir}" == "" ]]; then
+    echo "Invalid repo directory detected"
+    exit 1
+fi
+
+printf "\n******** Preparing temporary dir ********\n"
+if [[ -d "${tmpDir}" ]]; then
+    rm -rv "${tmpDir}"
+fi
+
+mkdir -pv "${tmpDir}"
+
+printf "\n******** Generating Go code ********\n"
+${flatc} --go -o "${tmpDir}" "${repoDir}"/third_party/flatbuffers-c-bridge/cmake-build/_deps/flatbuffers-*-src/reflection/reflection.fbs
+echo "Generated files:"
+ls "${tmpDir}/reflection"
+
+printf "\n******** Updating repo code ********\n"
+rsync -av --delete \
+    "${tmpDir}/reflection/" \
+    "${repoDir}/internal/generator/flatbuffersc/reflection/"
+
+printf "\n******** Removing temporary dir ********\n"
+rm -rv "${tmpDir}"
