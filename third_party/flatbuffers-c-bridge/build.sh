cb801f728f961485f1375d806fe284e1de7b74df
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-06-14 18:22:03 +0200

    flatbuffers-c-bridge build.sh - windows compatibility

--- third_party/flatbuffers-c-bridge/build.sh
+++ third_party/flatbuffers-c-bridge/build.sh
@@ -5,36 +5,54 @@ srcDir=$(dirname "${BASH_SOURCE[0]}")
 buildDir=${srcDir}/cmake-build
 installDir=${1:-${buildDir}}
 
-function build() {
-    echo "******** Configuring ********"
-    cmake -S "$srcDir" -B "$buildDir" -DCMAKE_BUILD_TYPE=Release
+buildType=Release
 
-    echo "******** Building ********"
-    cmake --build "$buildDir" \
-        --target flatbuffers \
-        --target flatbuffers-c-bridge \
-        --target flatbuffers-c-bridge-flatc \
-        --target flatbuffers-c-bridge-test \
-        -- -j
+libPrefix=lib
+libExt=.a
+exeExt=
+configArgs="-DCMAKE_BUILD_TYPE=${buildType}"
+buildArgs="-- -j"
+buildOutputDir=
+if [[ "$(uname)" == MINGW* ]] || [[ "$(uname)" == CYGWIN* ]]; then
+    exeExt=.exe
+
+    # MinGW build
+    configArgs+=' -G "MinGW Makefiles"'
+
+    # MSVC build - CGO linking fails with "undefined reference to '__CxxFrameHandler4'"
+    #  libExt=.lib
+    #  configArgs="-A x64"
+    #  buildOutputDir=/${buildType}
+    #  # buildArgs="-- /m"    fails with "error MSB1008: Only one project can be specified."
+fi
+
+function build() {
+    echo "******** Configuring & building ********"
+    set -x
+    # need to use eval because of quotes in configArgs... bash is just wonderful...
+    eval "cmake -S \"$srcDir\" -B \"$buildDir\" $configArgs"
+
+    # Note: flatbuffers-c-bridge-test implies flatbuffers, flatbuffers-c-bridge and flatbuffers-c-bridge-flatc
+    # We don't specify them explicitly to be compatible with MSVC which allows only one target per cmake call...
+    cmake --build "$buildDir" --config ${buildType} --target flatbuffers-c-bridge-test ${buildArgs}
+    set +x
 }
 
 function install() {
     echo "******** Collecting artifacts ********"
-    if [[ "${installDir}" != "${buildDir}" ]]; then
-        echo "Copying from $buildDir to $installDir:"
+    if [[ "${installDir}" != "${buildDir}${buildOutputDir}" ]]; then
+        echo "Copying from ${buildDir}${buildOutputDir} to ${installDir}:"
+        cp "${buildDir}${buildOutputDir}"/${libPrefix}flatbuffers-c-bridge${libExt} "$installDir"
+        cp "${buildDir}${buildOutputDir}"/${libPrefix}flatbuffers-c-bridge-flatc${libExt} "$installDir"
     fi
-    rsync -v \
-        "$buildDir"/libflatbuffers-c-bridge.a \
-        "$buildDir"/libflatbuffers-c-bridge-flatc.a \
-        "$buildDir"/_deps/flatbuffers-*-build/libflatbuffers.a \
-        "$installDir"
+    cp "${buildDir}"/_deps/flatbuffers-*-build${buildOutputDir}/${libPrefix}flatbuffers${libExt} "$installDir"
     echo "The compiled libraries can be found here:"
-    ls -alh "$buildDir"/libflatbuffers*.a
+    ls -alh "$installDir"/${libPrefix}flatbuffers*${libExt}
 }
 
 function test() {
     echo "******** Testing ********"
-    (cd "$buildDir" && ./flatbuffers-c-bridge-test)
+    (cd "${buildDir}${buildOutputDir}" && ./flatbuffers-c-bridge-test${exeExt})
 }
 
 build
