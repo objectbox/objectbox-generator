7b2c09396ab62a50b1b64dd930f9519d925b1f72
Markus <markus@greenrobot>
2020-06-15 18:17:46 +0200

    fix license headers for first public release

--- third_party/flatbuffers-c-bridge/src/flatc_main.cpp
+++ third_party/flatbuffers-c-bridge/src/flatc_main.cpp
@@ -1,4 +1,21 @@
-// TODO license
+/*
+ * Copyright (C) 2020 ObjectBox Ltd. All rights reserved.
+ * https://objectbox.io
+ *
+ * This file is part of ObjectBox Generator.
+ *
+ * ObjectBox Generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * ObjectBox Generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ObjectBox Generator.  If not, see <http://www.gnu.org/licenses/>.
+ */
 // This file is heavily based on flatbuffers src/flatc_main.cpp - it exposes the main() function
 
 #include <flatbuffers/flatc.h>
