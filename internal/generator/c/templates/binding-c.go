54581dd6a1752e5982cf235f2f59006f3f7e596c
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-10-23 13:34:23 +0200

    C generated code - fix const qualifiers

--- internal/generator/c/templates/binding-c.go
+++ internal/generator/c/templates/binding-c.go
@@ -139,7 +139,7 @@ static bool {{$entity.Meta.CName}}_from_flatbuffer(const void* data, size_t size
 
 	const uint8_t* table = (const uint8_t*) data + __flatbuffers_uoffset_read_from_pe(data);
 	assert(table);
-	flatbuffers_voffset_t *vt = (flatbuffers_voffset_t*) (table - __flatbuffers_soffset_read_from_pe(table));
+	const flatbuffers_voffset_t* vt = (const flatbuffers_voffset_t*) (table - __flatbuffers_soffset_read_from_pe(table));
 	flatbuffers_voffset_t vs = __flatbuffers_voffset_read_from_pe(vt);
 
 	// variables reused when reading strings and vectors
@@ -272,7 +272,7 @@ static void* {{.FileIdentifier}}_get_object(OBX_box* box, obx_id id, void* (*fro
     if (!tx) return NULL;
 
     void* result = NULL;
-    void* data;
+    const void* data;
     size_t size;
     if (obx_box_get(box, id, &data, &size) == OBX_SUCCESS) {
         result = from_flatbuffer(data, size);
