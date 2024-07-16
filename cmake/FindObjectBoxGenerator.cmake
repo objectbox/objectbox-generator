41a187fe513c7789a32802aea63dc6ebf6ce92c5
Daniel Adler <noreply+daniel@objectbox.io>
2024-07-16 17:17:36 +0200

    cmake find module: improved compatibility with CMake 3.11

--- cmake/FindObjectBoxGenerator.cmake
+++ cmake/FindObjectBoxGenerator.cmake
@@ -11,19 +11,26 @@ set(ObjectBoxGenerator_FETCH_HASH_Windows SHA256=2a06d06567524c9fa8bd815007a9c8a
 set(ObjectBoxGenerator_FETCH_HASH_macOS SHA256=2a06d06567524c9fa8bd815007a9c8a304792b7c0e49fade775a7553026b419a)
 set(ObjectBoxGenerator_FETCH_DIR ${CMAKE_BINARY_DIR}/ObjectBoxGenerator-Fetch)
 set(ObjectBoxGenerator_FETCH_BASEURL "https://github.com/objectbox/objectbox-generator/releases/download")
+set(ObjectBoxGenerator_INSTALL_DIR ${CMAKE_BINARY_DIR}/ObjectBoxGenerator-Install)
 
 include(FindPackageHandleStandardArgs)
 
 # Find program also in fetch dir.
-find_program(ObjectBoxGenerator_EXECUTABLE objectbox-generator PATHS ${ObjectBoxGenerator_FETCH_DIR} NO_CACHE)
+find_program(ObjectBoxGenerator_EXECUTABLE objectbox-generator PATHS ${ObjectBoxGenerator_INSTALL_DIR} NO_CACHE)
 
 if(ObjectBoxGenerator_EXECUTABLE)
   execute_process(
     COMMAND ${ObjectBoxGenerator_EXECUTABLE} -version 
     OUTPUT_VARIABLE Output 
     OUTPUT_STRIP_TRAILING_WHITESPACE 
-    ECHO_ERROR_VARIABLE 
-    COMMAND_ERROR_IS_FATAL ANY)
+    RESULT_VARIABLE ExitStatus
+    # 3.19: COMMAND_ERROR_IS_FATAL ANY
+  )
+  if(NOT ${ExitStatus} EQUAL 0) 
+    message(WARNING "Unable to query version on ${ObjectBoxGenerator_EXECUTABLE}")
+    return()
+  endif()
+
   string(REGEX REPLACE ".* v\([0-9\.]*\).*" "\\1" ObjectBoxGenerator_VERSION ${Output})
   find_package_check_version(${ObjectBoxGenerator_VERSION} ObjectBoxGenerator_VERSION_OK)
   if(NOT ${ObjectBoxGenerator_VERSION_OK}) 
@@ -35,7 +42,7 @@ endif()
   
 if(OBX_GENERATOR_ALLOW_FETCH AND ObjectBoxGenerator_FETCH_REQUIRED)
   message(STATUS "ObjectBox-Generator Fetch: Executable not found, attempting to download to build directory and prepare for execution (to disable behaviour set OBX_GENERATOR_ALLOW_FETCH to OFF)")
-  if (CMAKE_HOST_LINUX AND CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+  if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
     set(ObjectBoxGenerator_FETCH_ARCH Linux)
   elseif (CMAKE_HOST_WIN32 AND CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
     # TODO: check windows
@@ -63,10 +70,12 @@ if(OBX_GENERATOR_ALLOW_FETCH AND ObjectBoxGenerator_FETCH_REQUIRED)
         EXPECTED_HASH "${ObjectBoxGenerator_FETCH_HASH_${ObjectBoxGenerator_FETCH_ARCH}}"
   )
   message(STATUS "ObjectBox-Generator Fetch: Unpacking archive file ${ObjectBoxGenerator_FETCH_PATH} to ${ObjectBoxGenerator_UNPACK_FILE}")
-  file(ARCHIVE_EXTRACT INPUT ${ObjectBoxGenerator_FETCH_PATH} DESTINATION ${ObjectBoxGenerator_FETCH_DIR} VERBOSE)
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf ${ObjectBoxGenerator_FETCH_PATH} WORKING_DIRECTORY ${ObjectBoxGenerator_FETCH_DIR})
+  # 3.18: file(ARCHIVE_EXTRACT INPUT ${ObjectBoxGenerator_FETCH_PATH} DESTINATION ${ObjectBoxGenerator_FETCH_DIR} VERBOSE)
   message(STATUS "ObjectBox-Generator Fetch: Make file ${ObjectBoxGenerator_UNPACK_FILE} executable, and retry location..")
-  file(CHMOD ${ObjectBoxGenerator_UNPACK_FILE} PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
-  find_program(ObjectBoxGenerator_EXECUTABLE objectbox-generator PATHS ${ObjectBoxGenerator_FETCH_DIR})
+  file(COPY ${ObjectBoxGenerator_UNPACK_FILE} DESTINATION ${ObjectBoxGenerator_INSTALL_DIR} FILE_PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+  # 3.19: file(CHMOD ${ObjectBoxGenerator_UNPACK_FILE} PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+  find_program(ObjectBoxGenerator_EXECUTABLE objectbox-generator PATHS ${ObjectBoxGenerator_INSTALL_DIR})
 endif()
 
 find_package_handle_standard_args(ObjectBoxGenerator 
@@ -95,12 +104,15 @@ function (add_schema_files)
 
   foreach(SCHEMA_FILE ${ARG_SCHEMA_FILES})
     
-    cmake_path(ABSOLUTE_PATH SCHEMA_FILE OUTPUT_VARIABLE schema_filepath)
+    # 3.20: cmake_path(ABSOLUTE_PATH SCHEMA_FILE OUTPUT_VARIABLE schema_filepath)
+    set(schema_filepath ${CMAKE_CURRENT_SOURCE_DIR}/${SCHEMA_FILE})
 
     string(REGEX REPLACE "\.fbs$" ".obx.cpp" cppfile ${base_dir}/${SCHEMA_FILE})
     string(REGEX REPLACE "\.fbs$" ".obx.hpp" hppfile ${base_dir}/${SCHEMA_FILE})
     
-    cmake_path(GET cppfile PARENT_PATH out_dir)
+    # 3.20: cmake_path(GET cppfile PARENT_PATH out_dir)
+    string(REGEX REPLACE "/[^/]*$" "" out_dir ${cppfile}) 
+
     if (NOT ARG_INSOURCE)
       file(MAKE_DIRECTORY ${out_dir})
     endif()
