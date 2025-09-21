67caef3a74692494737776bc854943d6aef97e5b
Markus <markus@greenrobot>
2025-09-21 15:53:43 +0200

    Int test common.cmake: fetch ObjectBox 4.3.1

--- test/integration/cmake/projects/common.cmake
+++ test/integration/cmake/projects/common.cmake
@@ -8,7 +8,7 @@ include(FetchContent)
 FetchContent_Declare(
     objectbox
     GIT_REPOSITORY https://github.com/objectbox/objectbox-c.git
-    GIT_TAG        v4.2.0
+    GIT_TAG        v4.3.1
 )
 FetchContent_MakeAvailable(objectbox)
 
